9db9aacd8c7da28aeba60ad93fd42e1332196861
MartPc <mpinzonc07@gmail.com>
2023-11-08 08:29:50 -0500

    conexión

--- src/database/config.js
+++ src/database/config.js
@@ -1,12 +1,13 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize({
-    dialect:'mysql',
-    host:'localhost',
-    username:'root',
-    password:'OscarT.99',
-    database:'project'
-})
+const Sequelize = require('sequelize');
+const mysql2 = require('mysql2');
+
+const sequelize = new Sequelize('db_project_aws', 'admin', 'Default2023', {
+  host: 'database-project.cz2io8dbm9oh.us-east-2.rds.amazonaws.com',
+  dialect: 'mysql',
+  dialectModule: mysql2,
+});
 
 sequelize
     .authenticate()
